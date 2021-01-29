fefa2f5baa8761a33fc93a347693f24127ccb74d
Sierraa0 <77305741+Sierraa0@users.noreply.github.com>
2021-01-29 15:50:02 -0600

    How to make a game

---
+++ HowToMakeAVideoGame/Assets/Scripts/Name.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Name : MonoBehaviour
+{
+    public string TypeName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        TypeName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + TypeName + " to the game.";
+    }
+}
